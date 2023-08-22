87b79da328637d10d231f4a6f0ed8d56255527ea
Phuoc <nguyenducphuoc0802@gmail.com>
2023-08-22 21:22:10 +0700

    Initial commit

---
+++ app/src/main/java/com/example/jsonobject/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.jsonobject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tvInfo;
+
+    ImageButton imgVn, imgEn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AnhXa();
+
+        new ReadJSON().execute("https://www.facebook.com/");
+    }
+
+    private class ReadJSON extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            StringBuilder content = new StringBuilder();
+            try {
+                URL url = new URL(strings[0]);
+                InputStreamReader inputStreamReader = new InputStreamReader(url.openConnection().getInputStream());
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    content.append(line);
+                }
+
+            } catch (MalformedURLException e) {
+                throw new RuntimeException(e);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+            return content.toString();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            Toast.makeText(MainActivity.this, s, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void AnhXa() {
+        tvInfo = (TextView) findViewById(R.id.textViewInfo);
+        imgEn = (ImageButton) findViewById(R.id.imageButtonen);
+        imgVn = (ImageButton) findViewById(R.id.imageButtonvn);
+    }
+}
